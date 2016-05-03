31f68a293a39aa3c9a85143452a1da8aede57a79
Emma Hoffman <emmahoffman720@gmail.com>
2016-05-03 19:17:29 +0000

    Validating the picture field on the gram

--- spec/factories.rb
+++ spec/factories.rb
@@ -10,7 +10,9 @@ FactoryGirl.define do
 
   factory :gram do
     message "hello"
+    picture { fixture_file_upload(Rails.root.join('spec', 'fixtures', 'picture.png'), 'image/png') }
+    
     association :user
   end
-  
+
 end
